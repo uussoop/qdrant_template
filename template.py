ee630c77ee10f18ba7fdfa7af88fcc973a0ff69a
parsa <hparsa1378@gmail.com>
2023-09-06 18:36:11 +0330

    template search and insert update to match needs

--- template.py
+++ template.py
@@ -16,9 +16,9 @@ QDRANT_HOST = "http://0.0.0.0:6333/"
 qd_client = qdrant_client.QdrantClient(url=QDRANT_HOST)
 
 
-def format_embedding_to_db_structure(data):
+def format_embedding_to_db_structure(data, payload, collection_name):
     embeddings = openai.Embedding.create(
-        input=data["chat analysis with detected patterns"],
+        input=data,
         model="text-embedding-ada-002",
         api_key=OPENAI_API_KEY,
     )
@@ -26,31 +26,28 @@ def format_embedding_to_db_structure(data):
 
     if len(data1) > 0:
         structured = []
+
+        try:
+            vc = qd_client.get_collection(collection_name=collection_name).vectors_count
+        except:
+            vc = 0
         for d in data1:
+            # print("collection none existant")
             structured.append(
                 PointStruct(
-                    id=qd_client.get_collection(collection_name="test").vectors_count,
+                    id=vc,
                     vector=d["embedding"],
-                    payload=data,
+                    payload=payload[d["index"]],
                 )
             )
-        return structured, len(data1[0]["embedding"])
+            vc += 1
+        return structured, len(structured[0].vector)
     else:
         return None
 
 
-def template_filler(analysis, future_pattern):
-    template = {
-        "chat analysis with detected patterns": "",
-        "future pattern of market": "",
-    }
-    template["chat analysis with detected patterns"] = analysis
-    template["future pattern of market"] = future_pattern
-    return template
-
-
-def insert_data(collection_name, data):
-    points = format_embedding_to_db_structure(data)
+def insert_data(collection_name, payload, data):
+    points = format_embedding_to_db_structure(data, payload, collection_name)
     if collection_name not in [c.name for c in qd_client.get_collections().collections]:
         qd_client.recreate_collection(
             collection_name=collection_name,
@@ -58,17 +55,20 @@ def insert_data(collection_name, data):
                 size=points[1], distance=models.Distance.COSINE  # type: ignore
             ),
         )
-    qd_client.upsert(collection_name=collection_name, wait=True, points=points[0])  # type: ignore
+    for p in points[0]:  # type: ignore
+        qd_client.upsert(collection_name=collection_name, wait=True, points=[p])  # type: ignore
     return True
 
 
-def search_data(search_term, limit=3):
+def search_data(search_term, collection_name, limit=3):
     embeddings = openai.Embedding.create(
         input=search_term, model="text-embedding-ada-002", api_key=OPENAI_API_KEY
     )
     data = embeddings["data"][0]["embedding"]  # type: ignore
+    if collection_name not in [c.name for c in qd_client.get_collections().collections]:
+        return None
     search_result = qd_client.search(
-        collection_name="test",
+        collection_name=collection_name,
         query_vector=data,
         limit=limit,
         with_payload=True,
@@ -85,7 +85,11 @@ def search_data(search_term, limit=3):
     ]
 
 
+# if __name__ == "__main__":
 # example usage
 # before using : docker compose up -d
-# insert_data("test", template_filler("pattern 1", "predction"))
-# search_data("a12g21222")
+# insert_data("test", "data representation", "data to be embeded")
+# s = search_data(" BCNGarden project", "articles")
+# for i in s:  # type: ignore
+#     print(i["payload"])
+#     break
